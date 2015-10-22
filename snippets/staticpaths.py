b5689d4e3f9549bfdc7031e56fd90b626e290bc7
snehalbhayani <snehal.bhayani@yahoo.com>
2015-10-22 11:39:10 +0530

    Update staticpaths.py

--- snippets/staticpaths.py
+++ snippets/staticpaths.py
@@ -1,4 +1,4 @@
 URL_FOR_PRONUNICATION_FILES='https://ssl.gstatic.com/dictionary/static/sounds/de/0/'
 ENDPOINT_FOR_PRONUNCIATION_CACHE=''
 CORPUS_FOR_EXAMPLES='brown'
-NEO4J_SERVER_DIST_PATH='/home/snehal/Downloads/neo4j-community-2.2.3/'
+NEO4J_SERVER_DIST_PATH='/home/snehal/neo4j-community-2.2.3/'
